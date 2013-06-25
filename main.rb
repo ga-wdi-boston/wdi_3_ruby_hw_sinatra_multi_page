42d8218ffa468f1ecc38c2dc782ca47a9e542da0
Christopher Wright <cmwright33@gmail.com>
2013-06-25 19:46:28 -0400

    editied the movie get function

--- main.rb
+++ main.rb
@@ -2,26 +2,34 @@ require 'pry'
 require 'sinatra'
 require 'sinatra/reloader' if development?
 
+
 # This should list all the movies
 get '/movies' do
-  movie_file = File.new('movies.csv', 'r')
+  movies_file = File.new('movies.csv', 'r')
   @movies = []
-  movie_file.each do |movie|
+  movies_file.each do |movie|
     @movies << movie.split(',')
-
   end
-  movie_file.close
+  movies_file.close
   erb :movies
 end
 
 # This should show a single movie
 get '/movie/:name' do
+  @name = :name
+  movie_file = File.new('movies.csv', 'r')
+  @movie = {}
+  movie_file.each do |movie, year|
+     p
+  end
   erb :movie
 end
 
 # This page should have a form to create a new movie, which will POST to /new_movie
 get '/new_movie' do
   erb :movie
+  @title = params[:title]
+
 end
 
 # Create a new movie by sending a POST request to this URL
