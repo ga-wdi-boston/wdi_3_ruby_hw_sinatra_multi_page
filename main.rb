80e3450481265f26e5426cec8f32dc8b53b9405b
Zachariah Reitano <zreitano@gmail.com>
2013-06-25 17:16:29 -0400

    Enter New Movie Form Appears

--- main.rb
+++ main.rb
@@ -15,12 +15,22 @@ end
 
 # This should show a single movie
 get '/movie/:name' do
+
   erb :movie
 end
 
 # This page should have a form to create a new movie, which will POST to /new_movie
 get '/new_movie' do
-  erb :movie
+  @name = params[:name]
+  @year = params[:year]
+  @director = params[:director]
+  @image_link = params[:image_link]
+  @box_office = params[:box_office]
+  erb :new_movie
+  add_movie = File.new('movies.csv', 'a+')
+  add_movie.puts("#{@name},#{@year},#{@director},#{@image_link},#{@box_office}")
+  add_movie.close
+  erb :new_movie
 end
 
 # Create a new movie by sending a POST request to this URL
