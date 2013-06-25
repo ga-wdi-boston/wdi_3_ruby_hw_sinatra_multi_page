30c5d2b2329bb08cfa1213a768b8691fa1b488ba
Christine Coulter <christinetcoulter@gmail.com>
2013-06-25 16:58:01 -0400

    set up for manual links test on main.rb file

--- main.rb
+++ main.rb
@@ -4,6 +4,16 @@ require 'sinatra/reloader' if development?
 
 # This should list all the movies
 get '/' do
+  @movies = ["http://upload.wikimedia.org/wikipedia/en/thumb/c/ce/Citizenkane.jpg/405px-Citizenkane.jpg,1585634",
+    "http://upload.wikimedia.org/wikipedia/en/8/88/Img_kingkong1.jpg,1700000",
+    "http://upload.wikimedia.org/wikipedia/en/d/de/Bvmovieposter.jpg,8551228",
+    "http://upload.wikimedia.org/wikipedia/commons/e/e5/Vertigomovie.jpg,14000000",
+    "http://upload.wikimedia.org/wikipedia/en/f/fc/Fight_Club_poster.jpg,100900000",
+    "https://upload.wikimedia.org/wikipedia/en/b/b9/Psycho_%281960%29.jpg",
+    "http://upload.wikimedia.org/wikipedia/en/2/21/ThirdManUSPoster.jpg,277549",
+    "http://upload.wikimedia.org/wikipedia/commons/9/9d/Casablanca%2C_title.JPG",
+    "http://upload.wikimedia.org/wikipedia/commons/d/d6/Rearwindowposter.jpg,36764313",
+    "http://upload.wikimedia.org/wikipedia/en/9/9e/Rebel_without_a_cause432.jpg,4500000"]
   erb :movies
 end
 
