e056749c8a4c3ece8f29593d3f8389e32dd08723
Ben Israeli <jisraeli@gmail.com>
2013-06-25 18:28:22 -0400

    stared at code for 45 minutes. changed something?

--- main.rb
+++ main.rb
@@ -1,20 +1,31 @@
 require 'pry'
 require 'sinatra'
 require 'sinatra/reloader' if development?
-
 # This should list all the movies
-get '/' do
+get '/allmovies' do
+  movie_file = File.new('movies.csv','r')
+  @movies = []
+  movie_file.each do |line|
+    @movies << line.split(',')
+  end
+  movie_file.close
   erb :movies
 end
 
 # This should show a single movie
 get '/movie/:name'
+  @name = params[:name]
+  movie_file = File.new('movies.csv','r')
+  movie_file =
+  #how to find 1) Loop through movie file until first thing thta contains movie title matches the title of the movie - what line is it on so i can retrieve info from that line to out put it?
   erb :movie
 end
 
 # This page should have a form to create a new movie, which will POST to /new_movie
 get '/new_movie' do
-  erb :movie
+
+
+  erb :new_movie
 end
 
 # Create a new movie by sending a POST request to this URL
