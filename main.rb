d7d4eef12124ca9f84fb31212495d9fd37003d30
Jamal Powell <jamalkpowell@gmail.com>
2013-06-25 18:22:44 -0400

    fixed main file, able to input text

--- main.rb
+++ main.rb
@@ -4,51 +4,46 @@ require 'sinatra/reloader' if development?
 
 # This should list all the movies
 get '/' do
-  movie = File.new('movies.csv', 'r')
   @movies = []
+  movie = File.new('movies.csv', 'r')
   movie.each do |title|
     @movies << title.split(',')
   end
   movie.close
-    erb :movies
+   erb :movies
 end
 
 # This should show a single movie
 get '/movie/:name' do
+  @name = params[:name]
+  @info = []
   erb :movie
 end
 
-get '/new_movie' do
-  erb :new_movie
-end
+
 
 # This page should have a form to create a new movie, which will POST to /new_movie
 get '/new_movie' do
-
   erb :new_movie
 end
 
+
 # Create a new movie by sending a POST request to this URL
 post '/new_movie' do
-  @movie_title = params[:title]
-  @movie = paramas[:movie]
   movie_title = File.new('movies.csv', 'a+')
-  movie_title.puts("#{@new_movie}, #{@movie}")
+  @title = params[:title]
+  @year = params[:year]
+  @director = params[:director]
+  @link = params[:link]
+  @revenue = params[:revenue]
+  movie_title.puts("#{@title}, #{@year}, #{@director}, #{@link}, #{@revenue}")
   movie_title.close
-  redirect to ("/movies")
+  redirect to ("/movies/#{URI::encode(@title)}")
 
 end
 
   #This will send you to the newly created movie
   #redirect to("/movies/#{@title}")
-
-post '/navigate' do
-  case params[:destination].downcase
-    when "new movie" then redirect to("/new_movie")
-    when "movies" then redirect to ("/")
-  end
-  redirect to("/")
-end
 
 
 
