740b73a599ea64e2633a7611929856925e6c92de
Zachariah Reitano <zreitano@gmail.com>
2013-06-25 17:36:44 -0400

    Movie Added, Takes to new page

--- main.rb
+++ main.rb
@@ -14,29 +14,33 @@ get '/' do
 end
 
 # This should show a single movie
-get '/movie/:name' do
-
+get '/movie/:title' do
+  @title = params[:title]
+  list_of_movies = File.new('movies.csv', 'r')
+  @movies = []
+  list_of_movies.each do |line|
+    @movies << line[0]
+  end
+  list_of_movies.close
   erb :movie
 end
 
 # This page should have a form to create a new movie, which will POST to /new_movie
 get '/new_movie' do
-  @name = params[:name]
-  @year = params[:year]
-  @director = params[:director]
-  @image_link = params[:image_link]
-  @box_office = params[:box_office]
-  erb :new_movie
-  add_movie = File.new('movies.csv', 'a+')
-  add_movie.puts("#{@name},#{@year},#{@director},#{@image_link},#{@box_office}")
-  add_movie.close
   erb :new_movie
 end
 
 # Create a new movie by sending a POST request to this URL
 post '/new_movie' do
   @title = params[:title]
-
+  @year = params[:year]
+  @director = params[:director]
+  @image_link = params[:image_link]
+  @box_office = params[:box_office]
+  erb :new_movie
+  add_movie = File.new('movies.csv', 'a+')
+  add_movie.puts("#{@title},#{@year},#{@director},#{@image_link},#{@box_office}")
+  add_movie.close
   #This will send you to the newly created movie
-  redirect to("/movies/#{@title}")
+  redirect to("/movie/#{@title}")
 end
