3deba3380791f6c22e4f53c9748e0e9d84482827
Theodore Cleveland <tcclevela@gmail.com>
2013-06-25 17:19:08 -0400

    changed home directory get to only pull title

--- main.rb
+++ main.rb
@@ -7,41 +7,31 @@ get '/' do
 	@movies = []
 	movie_links = File.new("movies.csv", "r")
 	movie_links.each do |line|
-		@movies << line.split(",")
+		@movies << line.split(",")[0]
 	end
 	movie_links.close
 	erb :movies
 end
 
-get '/new_movie' do
-	erb :new_movie
-end
-
-post '/new_movie' do
-	movie_links = File.new("movies.csv", "a+")
-	@title = params[:title]
-	@year = params[:title]
-	@director = params[:director]
-	@img_link = params[:img_link]
-	movie_links.puts("\n#{@title}, #{@year}, #{@director}, #{@img_link}")
-	movie_links.close
-	redirect to('/')
-end
 # This should show a single movie
-get '/movie/:name' do
+get '/movies/:name' do
 
   	erb :movie
 end
 
 # This page should have a form to create a new movie, which will POST to /new_movie
 get '/new_movie' do
-  erb :movie
+	erb :new_movie
 end
 
 # Create a new movie by sending a POST request to this URL
 post '/new_movie' do
-  @title = params[:title]
-
-  #This will send you to the newly created movie
-  redirect to("/movies/#{@title}")
+	movie_links = File.new("movies.csv", "a+")
+	@title = params[:title]
+	@year = params[:title]
+	@director = params[:director]
+	@img_link = params[:img_link]
+	movie_links.puts("#{@title}, #{@year}, #{@director}, #{@img_link}")
+	movie_links.close
+	redirect to("/movie/#{@title}")
 end
