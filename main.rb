0779d6601a0b3090fca486f977b5fa03ecc8ee88
mvanger <mvanger@nyu.edu>
2013-06-25 16:46:49 -0400

    Added movie list to index page

--- main.rb
+++ main.rb
@@ -4,13 +4,19 @@ require 'sinatra/reloader' if development?
 
 # This should list all the movies
 get '/' do
+  movie_file = File.new('movies.csv', 'r')
+  @movies = []
+  movie_file.each do |line|
+    @movies << line.split(',')
+  end
+  movie_file.close
   erb :movies
 end
 
 # This should show a single movie
-get '/movie/:name'
-  erb :movie
-end
+# get '/movie/:name'
+#   erb :movie
+# end
 
 # This page should have a form to create a new movie, which will POST to /new_movie
 get '/new_movie' do
