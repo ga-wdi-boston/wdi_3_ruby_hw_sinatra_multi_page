059cccd4dc1d056045fbc5aea10e7971f9c5d1d0
Wesley Carr <surfswell17@gmail.com>
2013-06-25 19:11:10 -0400

    Pulling info into an array from movies.csv & routing to individual pages

--- main.rb
+++ main.rb
@@ -4,30 +4,36 @@ require 'sinatra/reloader' if development?
 
 # This should list all the movies
 get '/' do
-  movies_file = File.new('movies.csv', 'r')
   @movies = []
+  movies_file = File.new('movies.csv', 'r')
   movies_file.each do |movie|
-    @movies << movie.split(',')
+    @movies << movie.split(',')[0]
   end
   movies_file.close
   erb :movies
 end
 
-
-# This should show a single movie
-get '/movie/:name' do
+# This should show a single movie being pulled from the movies array
+get '/movie/:title' do
+  @title = params[:title]
+  @info = []
+  movies_file = File.new("movies.csv", "r")
+  movies_file.each do |line|
+    if line.split(',')[0] == @title then
+      @info = line.split(',')
+    end
+  end
+    movies_file.close
   erb :movie
 end
-
-# This page should have a form to create a new movie, which will POST to /new_movie
+# Send to create movie entry page
 get '/new_movie' do
-  erb :movie
+  erb :new_movie
 end
 
 # Create a new movie by sending a POST request to this URL
 post '/new_movie' do
   @title = params[:title]
-
   #This will send you to the newly created movie
   redirect to("/movies/#{@title}")
 end
