447de017bc5f113941a621d1068c3a22d83e618a
nicklombardi <nicklombardi@gmail.com>
2013-06-25 19:59:32 -0400

    created new movie page

--- main.rb
+++ main.rb
@@ -29,13 +29,19 @@ end
 
 # This page should have a form to create a new movie, which will POST to /new_movie
 get '/new_movie' do
-  erb :movie
+  erb :new_movie
 end
 
 # Create a new movie by sending a POST request to this URL
 post '/new_movie' do
   @title = params[:title]
-
+  @year = params[:year]
+  @director = params[:director]
+  @picture_url = params[:picture_url]
+  @revenue = params[:revenue]
+  f = File.new('movies.csv', 'a+')
+  f.puts("#{@title},#{@link}")
+  f.close
   #This will send you to the newly created movie
   redirect to("/movies/#{@title}")
 end
