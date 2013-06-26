c934def61adfe990f028dd0e2bd849bbab7e177f
Ben Israeli <jisraeli@gmail.com>
2013-06-25 22:36:36 -0400

    no change i can identify

--- main.rb
+++ main.rb
@@ -13,20 +13,21 @@ get '/allmovies' do
 end
 
 # This should show a single movie
-get '/movie/:name'
-  @name = params[:name]
-  movie_file = File.new('movies.csv','r')
-  movie_file =
-  #how to find 1) Loop through movie file until first thing thta contains movie title matches the title of the movie - what line is it on so i can retrieve info from that line to out put it?
-  erb :movie
-end
+#get '/movie/:name'
+#   @name = params[:name]
+#   movie_file = File.new('movies.csv','r')
+#   loop movie.file.each do |movie|
+#     until params[:name] =
+#   #how to find 1) Loop through movie file until first thing thta contains movie title matches the title of the movie - what line is it on so i can retrieve info from that line to out put it?
+#   erb :movie
+# end
 
 # This page should have a form to create a new movie, which will POST to /new_movie
-get '/new_movie' do
+#get '/new_movie' do
 
 
-  erb :new_movie
-end
+  #erb :new_movie
+#end
 
 # Create a new movie by sending a POST request to this URL
 post '/new_movie' do
