ba4faac345c2187e229f00594c619f68b09858a3
Anuj Jhunjhunwala <anuj.jhunjhunwala@gmail.com>
2013-06-25 21:48:07 -0400

    Fixed ability to show movie on own page, adding ability to add new movies

--- main.rb
+++ main.rb
@@ -13,38 +13,48 @@ get '/' do
   erb :movies
 end
 
-binding.pry
-
-
 # This should show a single movie
-get '/movies/:name' do
-  [@movies_list][0] = params[:title]
-  [@movies_list][1] = params[:year]
-  [@movies_list][2] = params[:director]
-  [@movies_list][3] = params[:image]
-  [@movies_list][4] = params[:revenue]
-
-  # @title = [@movies_list][0]
-  # @year = [@movies_list][1]
-  # @director = [@movies_list][2]
-  # @image = [@movies_list][3]
-  # @revenue = [@movies_list][4]
-
+get '/movie/:title' do
+  @title = params[:title]
+  @details = []
+  list_file = File.new('movies.csv', 'r')
+  list_file.each do |movie|
+    if movie.split(',')[0] == @title then
+      @details = movie.split(',')
+  end
+end
   erb :movie
 end
 
+# This page should have a form to create a new movie, which will POST to /new_movie
+get '/new_movie' do
+  # @title = params[:title]
+  # @year = params[:year]
+  # @director = params[:director]
+  # @image = params[:image]
+  # @revenue = params[:revenue]
 
+  # f = File.new('movies.csv', 'a+')
+  # f.puts("#{@title},#{@year},#{@director},#{@image},#{@revenue}")
+  # f.close
+  # redirect to('/')
+  erb :new_movie
+end
 
-# # This page should have a form to create a new movie, which will POST to /new_movie
-# get '/new_movie' do
+  #   @link_name = params[:name]
+  # @link = params[:link]
+  # f = File.new('links.csv', 'a+')
+  # f.puts("#{@link_name},#{@link}")
+  # f.close
+  # redirect to("/links")
 #   erb :movie
 # end
 
-# # Create a new movie by sending a POST request to this URL
-# post '/new_movie' do
-#   @title = params[:title]
+# Create a new movie by sending a POST request to this URL
+post '/new_movie' do
+  @title = params[:title]
 
-#   #This will send you to the newly created movie
-#   redirect to("/movies/#{@title}")
-# end
+  #This will send you to the newly created movie
+  redirect to("/movies/#{@title}")
+end
 
