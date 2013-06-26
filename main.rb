3af16338a2fc99853baf60538f420adc9fb0bf0b
Alina Guzman <alinochka8@gmail.com>
2013-06-25 20:11:22 -0400

    Set up the Post method to write into movie file with new input, and set up the form and button on new_movie page.

--- main.rb
+++ main.rb
@@ -21,18 +21,22 @@ get '/movie/:name' do
     movie_file.each do |movie_line|
       @movies << movie_line.split(",")
     end
+  movie_file.close
   erb :movie
 end
 
 # This page should have a form to create a new movie, which will POST to /new_movie
 get '/new_movie' do
-  erb :movie
+  erb :new_movie
 end
 
 # Create a new movie by sending a POST request to this URL
 post '/new_movie' do
   @title = params[:title]
-
+  @movie = params[:movie]
+  movie_file = File.new("movies.csv", "a+")
+  movie_file.puts("\n#{@name},#{@year},#{@director},#{@image},#{@budget}")
+  movie_file.close
   #This will send you to the newly created movie
   redirect to("/movies/#{@title}")
 end
