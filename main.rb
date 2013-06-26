ff48fc4235c4a9af3b2e4d0b6a4292cb379e408a
egong <egong82@gmail.com>
2013-06-25 21:49:47 -0400

    did best without help

--- main.rb
+++ main.rb
@@ -4,16 +4,31 @@ require 'sinatra/reloader' if development?
 
 # This should list all the movies
 get '/' do
+  link_file = File.new('movies.csv', 'r')
+  @links = []
+  link_file.each do |line|
+    @links << line.split(',')
+  end
+  link_file.close
   erb :movies
 end
 
 # This should show a single movie
-get '/movie/:name'
+get '/movie/:name' do
   erb :movie
 end
 
-# This page should have a form to create a new movie, which will POST to /new_movie
 get '/new_movie' do
+  erb :movie
+  end
+
+# This page should have a form to create a new movie, which will POST to /new_movie
+post '/new_movie' do
+  @movie = params[:movie]
+  f = File.new('movies.csv', 'a+')
+  f.puts("#{@movie}")
+  f.close
+  redirect to("/")
   erb :movie
 end
 
