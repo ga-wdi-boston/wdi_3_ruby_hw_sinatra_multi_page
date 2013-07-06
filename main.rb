ce3b00a1e3cd3a69a03fa5c85d3eb591c616fbbe
Tim <tim.blonski@gmail.com>
2013-07-06 14:31:36 -0400

    Completed the rest of the lab. Added an 'add new movie page' that will display and write new movies to the movies.csv file.

--- main.rb
+++ main.rb
@@ -30,8 +30,6 @@ get '/movie/:name' do
   @name = params[:name]
   @movies = get_movie_hash
 
-  @single_movie = @movies[@name]
-
   erb :movie
 end
 
@@ -40,13 +38,32 @@ end
 
 # This page should have a form to create a new movie, which will POST to /new_movie
 get '/new_movie' do
-  erb :movie
+  erb :new_movie
 end
 
 # Create a new movie by sending a POST request to this URL
 post '/new_movie' do
-  @title = params[:title]
+  @title = params[:title].capitalize
+  @year = params[:year]
+  @picture = params[:picture]
+  @revenue = params[:revenue]
+
+  File.open('movies.csv', 'a') do |file|
+    file.write("#{@title},#{@year},#{@picture},#{@revenue}")
+  end
+
 
   #This will send you to the newly created movie
-  redirect to("/movies/#{@title}")
+  redirect to("/movie/#{@title}")
 end
+
+
+
+
+
+
+
+
+
+
+
